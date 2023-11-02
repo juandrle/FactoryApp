fe53e3260f33227810c4682ab44041a0de39ffa5
Julian Andrle <julian.andrle@student.hs-rm.de>
2023-11-02 15:14:45 +0100

    Initial Commit

---
+++ src/main/java/de/swtpro/factorybuilder/FactoryBuilderApplication.java
@@ -0,0 +1,13 @@
+package de.swtpro.factorybuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/swtpro/factorybuilder/FactoryBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package de.swtpro.factorybuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
