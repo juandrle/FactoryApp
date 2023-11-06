461c01fdc3ce7360cab7bd0b8764ae6bfd6880e1
julianandrle <julian.andrle@syntax-institut.de>
2023-11-06 17:00:47 +0100

    integrated threeJS

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import threeJS from './components/threeJS.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>DEMO :)</h1>
+      <threeJS></threeJS>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
