b0c93ef23e3f0c986a94d3ad4ce4e5c5384da8c8
Julian Andrle <julian.andrle@student.hs-rm.de>
2023-11-06 10:08:50 +0100

    testJenkins

--- src/main/java/de/swtpro/factorybuilder/FactoryBuilderApplication.java
+++ src/main/java/de/swtpro/factorybuilder/FactoryBuilderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FactoryBuilderApplication {
+    private boolean jenkinsWorking;
 
     public static void main(String[] args) {
         SpringApplication.run(FactoryBuilderApplication.class, args);
