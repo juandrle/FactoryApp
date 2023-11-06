032c7621168ef4b83f64581a68e0d9e2dab1f5bf
Julian Andrle <julian.andrle@student.hs-rm.de>
2023-11-06 10:26:09 +0100

    jacoco added for testcoverage

--- src/main/java/de/swtpro/factorybuilder/FactoryBuilderApplication.java
+++ src/main/java/de/swtpro/factorybuilder/FactoryBuilderApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FactoryBuilderApplication {
-    private boolean jenkinsWorking;
+    private final boolean jenkinsWorking = true;
 
     public static void main(String[] args) {
         SpringApplication.run(FactoryBuilderApplication.class, args);
