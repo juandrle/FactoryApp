f0d4e0c1987eeb74e212646efbbcf21386012e59
Lucas Bewersdorff <Lucas.Bewersdorff@student.hs-rm.de>
2023-11-12 16:45:05 +0100

    Added REST-API-Controller for a cube

---
+++ src/main/java/de/swtpro/factorybuilder/controller/CubeController.java
@@ -0,0 +1,23 @@
+package de.swtpro.factorybuilder.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CubeController {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CubeController.class);
+
+    @PostMapping(value = "/cube", produces = "application/json")
+    public ResponseEntity<String> setCubeJSON(@RequestBody String cubeJSON) {
+        LOGGER.info("JSON received");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
