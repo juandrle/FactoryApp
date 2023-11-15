58b5a3c8f29733bda050326f133f55f7ce4b82fe
Lucas Bewersdorff <Lucas.Bewersdorff@student.hs-rm.de>
2023-11-15 13:33:01 +0100

    Added Cube-Entity attributes

--- src/main/java/de/swtpro/factorybuilder/entity/Cube.java
+++ src/main/java/de/swtpro/factorybuilder/entity/Cube.java
@@ -1,12 +1,14 @@
 package de.swtpro.factorybuilder.entity;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Version;
 
 @Entity
-public class Cube {
+public class Cube implements Serializable{
     
     @Id
     @GeneratedValue
@@ -14,6 +16,19 @@ public class Cube {
 
     @Version
     private long version;
+
+    private int sideLength;
+
+    private int x;
+
+    private int y;
+    
+    private int z;
+
+    private String orientation;
+
+    private String modelId;
+
 
     public Cube() {
     }
@@ -32,6 +47,54 @@ public class Cube {
 
     public void setVersion(long version) {
         this.version = version;
+    }
+
+    public int getSideLength() {
+        return sideLength;
+    }
+
+    public void setSideLength(int sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+
+    public String getModelId() {
+        return modelId;
+    }
+
+    public void setModelId(String modelId) {
+        this.modelId = modelId;
     }
 
     
