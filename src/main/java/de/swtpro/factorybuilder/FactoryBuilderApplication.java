fd4355e218d98c6cb4efe26f54848e6dd3c5cc68
Julian Andrle <julian.andrle@student.hs-rm.de>
2023-11-16 20:10:47 +0100

    readded src of Spring, that was lost

---
+++ src/main/java/de/swtpro/factorybuilder/FactoryBuilderApplication.java
@@ -0,0 +1,14 @@
+package de.swtpro.factorybuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryBuilderApplication {
+    private final boolean jenkinsWorking = true;
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/swtpro/factorybuilder/FactoryBuilderApplicationTests.java
@@ -0,0 +1,9 @@
+package de.swtpro.factorybuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryBuilderApplicationTests {
+
+}
