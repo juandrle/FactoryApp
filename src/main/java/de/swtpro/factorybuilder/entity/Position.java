f905d0dd442c2e9a469f60e2f7ec8eaf896e28bd
Lucas Bewersdorff <Lucas.Bewersdorff@student.hs-rm.de>
2023-11-19 18:19:20 +0100

    added parameterized constructor to position class

--- src/main/java/de/swtpro/factorybuilder/entity/Position.java
+++ src/main/java/de/swtpro/factorybuilder/entity/Position.java
@@ -8,6 +8,12 @@ public class Position {
     
     private int z;
 
+    public Position(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     public int getX() {
         return x;
     }
