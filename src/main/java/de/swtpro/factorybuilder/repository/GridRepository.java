f6b7f1b5341a79ad2eb7114306eac57c1cf1808c
Julian Andrle <julian.andrle@student.hs-rm.de>
2023-12-13 11:02:15 +0100

    fixed current State of backend

--- src/main/java/de/swtpro/factorybuilder/repository/GridRepository.java
+++ src/main/java/de/swtpro/factorybuilder/repository/GridRepository.java
@@ -1,8 +1,12 @@
 package de.swtpro.factorybuilder.repository;
 
 import de.swtpro.factorybuilder.entity.Field;
+import de.swtpro.factorybuilder.utility.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GridRepository extends JpaRepository<Field, Long> {
+    Optional<Field> findByPos(Position pos);
 
 }
