1102318224122d762760825db91e5a1fee3ffe57
ocevi001 <oezguen.ceviz@student.hs-rm.de>
2023-12-17 22:51:17 +0100

    i give up

--- src/main/java/de/swtpro/factorybuilder/configuration/SecurityConfiguration.java
+++ src/main/java/de/swtpro/factorybuilder/configuration/SecurityConfiguration.java
@@ -1,57 +1,50 @@
 package de.swtpro.factorybuilder.configuration;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.User;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        @Bean 
-        PasswordEncoder passwordEncoder(){
-            return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        }
-        @Bean 
-        public UserDetailsService userDetailsService() {
-            UserBuilder userbuilder = User.withDefaultPasswordEncoder();
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.builder()
+                .username("joendhard")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
 
-            UserDetails user1 = userbuilder.username("joendhard").password("1234")
-                                .roles("USER").build();
-            
+        return new InMemoryUserDetailsManager(user1);
+    }
 
-            return new InMemoryUserDetailsManager(user1);
-        }
+    @Bean
+    public SecurityFilterChain filterChainApp(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/assets/*").permitAll()
+                .requestMatchers("/signup").permitAll()
+                .requestMatchers("/home", "/factory", "/create").hasRole("USER")
+                .anyRequest().authenticated())
+                .formLogin(withDefaults())
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")) // Use antMatchers here
+                .headers(headers -> headers.frameOptions().sameOrigin())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessUrl("/login"));
 
-        @Bean
-        SecurityFilterChain filterChainApp(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz -> authz 
-        .requestMatchers(HttpMethod.GET, "/assets/*").permitAll()
-        .requestMatchers("/signup").permitAll()
-        
-
-        .requestMatchers("/home", "/factory", "/create").hasRole("USER")
-        .anyRequest().authenticated())
-        .formLogin(withDefaults()) // oder httpBasic oder ... // optional Redirect auf Wunschseite nach Logut 
-
-        .logout().logoutUrl("/logout").logoutSuccessUrl("/frage"); 
-
-        return http.build(); 
+        return http.build();
     }
 }
-    
-
